4de17570b58c6137337e05c2e5c80ba7bb2c702e
Johnathon Sanders <johnathon.sanders@novu.com>
2014-07-29 11:01:12 -0500

    Add foundation of mocha, chai, sinon test framework

---
+++ test/backbone.manager_test.js
@@ -0,0 +1,6 @@
+describe('Backbone.Manager', function() {
+  describe('constructor', function(){
+    var router = new Backbone.Router();
+    var manager = new Backbone.Manager(router);
+  });
+});
---
+++ test/common.js
@@ -0,0 +1,13 @@
+global.chai = require("chai");
+global.should = require("chai").should();
+global.expect = require("chai").expect;
+global.AssertionError = require("chai").AssertionError;
+
+global.swallow = function (thrower) {
+    try {
+        thrower();
+    } catch (e) { }
+};
+
+var sinonChai = require("../lib/sinon-chai");
+chai.use(sinonChai);
